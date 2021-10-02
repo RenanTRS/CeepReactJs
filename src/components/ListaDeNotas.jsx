99f083d6c4cfb071ee6d6d5beeb49ec6522e78a1
Renan Souza <renantrsouza@gmail.com>
2021-10-02 11:39:57 -0400

    Create component

---
+++ src/components/ListaDeNotas.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+export default class ListaDeNotas extends Component {
+  render() {
+    return (
+      <ul>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Seu texto aqui...</p>
+          </section>
+        </li>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Seu texto aqui...</p>
+          </section>
+        </li>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Seu texto aqui...</p>
+          </section>
+        </li>
+      </ul>
+    );
+  }
+}
