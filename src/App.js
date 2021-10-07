1757fb147c51e79f9911e7aed6f3ca532521052b
Renan Souza <renantrsouza@gmail.com>
2021-10-07 11:50:59 -0400

    Add import for components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Component } from 'react';
 import FormularioCadastro from './components/FormularioCadastro';
-import ListaDeNotas from "./components/ListaDeNotas"
+import ListaDeNotas from "./components/ListaDeNotas";
+import './assets/index.css';
+import "./assets/App.css";
 
 class App extends Component{
   render(){
     return (
-      <section>
+      <section className="conteudo">
         <FormularioCadastro/>
 
         <ListaDeNotas/>
