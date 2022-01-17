95f50da2098c3d3892eb64ef4ebaff878f09c3ce
Renan Souza <renantrsouza@gmail.com>
2022-01-17 11:14:38 -0400

    Transforma a imagem svg em um component

--- src/components/CardNota.jsx
+++ src/components/CardNota.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 import "../assets/cardNota.css";
-import DeleteImg from '../assets/img/delete.svg';
+import {ReactComponent as DeleteImg} from '../assets/img/delete.svg';
 
 export function CardNota({task, text}){
     return (
         <section className="card-nota">
             <header className="card-nota_cabecalho">
                 <h3 className="card-nota_titulo">{task}</h3>
-                <img src={DeleteImg} alt="Delete" />
+                <DeleteImg />
             </header>
             <p className="card-nota_texto">{text}</p>
         </section>
