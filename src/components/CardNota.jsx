de9fd44fd70b4f7e2a05b57207117cbda2f14c9e
Renan Souza <renantrsouza@gmail.com>
2022-01-17 11:06:15 -0400

    import image

--- src/components/CardNota.jsx
+++ src/components/CardNota.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import "../assets/cardNota.css";
+import DeleteImg from '../assets/img/delete.svg';
 
 export function CardNota({task, text}){
     return (
         <section className="card-nota">
             <header className="card-nota_cabecalho">
                 <h3 className="card-nota_titulo">{task}</h3>
+                <img src={DeleteImg} alt="Delete" />
             </header>
             <p className="card-nota_texto">{text}</p>
         </section>
